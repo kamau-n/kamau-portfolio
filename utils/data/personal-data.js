a896041417c18fd95fc34c10939aaca707c266ab
Harris <bsclmr146620@spu.ac.ke>
2024-10-05 14:39:35 +0300

    Minor Upgrade

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,16 +2,16 @@ export const personalData = {
   name: "Kamau Harrison",
   profile: '/profile.jpg',
   designation: "Software Developer",
-  description: "My name is Kamau Harrison. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "My name is Kamau Harrison. I am a Software Engineering enthusiast. I am a faster learner, Curious and also a problem solver. Iam always eager to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on Java and Typescript and I love to do most of the things using the two.",
   email: 'kamaun629@gmail.com',
   phone: '+254 759 155 650',
   address: 'Nairobi, Kenya',
   github: 'https://github.com/kamau-n',
   facebook: 'https://www.facebook.com/ka mau/',
   linkedIn: 'https://www.linkedin.com/in/kamauharrison/',
-  twitter: 'https://twitter.com/kamau',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  twitter: 'https://twitter.com/NjorogeKam12559',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/kamaun',
+  leetcode: "https://leetcode.com/kamaun/",
+  devUsername: "said7388jjjj",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVyyyyRkB5Wer00/view?usp=sharingl"
 }
--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,14 +1,14 @@
 import ayla from '/public/image/ayla.jpg';
-import crefin from '/public/image/crefin.jpg';
+import crefin from '/public/image/crefin.png';
 import realEstate from '/public/image/real-estate.jpg';
-import travel from '/public/image/travel.jpg';
+import hamisha from '/public/image/hamisha.png';
 
 export const projectsData = [
     {
         id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
+        name: 'Farmers Eccomerce Application',
+        description: "The appplication was developed with kenyan farmers in mind, the application would help them markert their products in order to attract a customer/consumer base, the applicatio is easy to navigate..",
+        tools: ['Express', 'MSSQL', , 'TypeScript', 'Tailwind', 'Socket/UI', 'Nginx'],
         role: 'Backend Developer',
         code: '',
         demo: '',
@@ -16,34 +16,34 @@ export const projectsData = [
     },
     {
         id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
+        name: 'Hamisha Application',
+        description: 'The application was designed to people who are moving to a new city or country.',
+        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "TypeScript", "MySQL", "Nodemailer"],
         role: 'Full Stack Developer',
         code: '',
         demo: '',
-        image: travel,
+        image: hamisha,
     },
-    {
-        id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-        image: realEstate,
-    },
-    {
-        id: 4,
-        name: 'Newsroom Management',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
-        code: '',
-        demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
-    }
+    // {
+    //     id: 3,
+    //     name: 'AI Powered Real Estate',
+    //     description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
+    //     tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
+    //     code: '',
+    //     role: 'Full Stack Developer',
+    //     demo: '',
+    //     image: realEstate,
+    // },
+    // {
+    //     id: 4,
+    //     name: 'Newsroom Management',
+    //     description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
+    //     tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
+    //     code: '',
+    //     demo: '',
+    //     image: ayla,
+    //     role: 'Full Stack Developer',
+    // }
 ];
 
 
